da86024e9b1a83a0f863e9908cab471364796ffa
Arus Gevorgyan <gevorgyanarus@gmail.com>
2022-03-02 15:37:39 +0400

    forgot password styling

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-forgot-passvord',
@@ -7,7 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  
+
+
+  constructor() {
+     
+   }
 
   ngOnInit(): void {
   }
