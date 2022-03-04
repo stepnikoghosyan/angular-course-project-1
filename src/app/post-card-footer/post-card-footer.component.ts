6668c6971bf02ee75b7c31a5252e7cdbbe936f87
MariamikGhazaryan <mariamikghazaryan1997@gmail.com>
2022-03-04 20:55:40 +0400

    Changes in reset password and formatting.

--- src/app/post-card-footer/post-card-footer.component.ts
+++ src/app/post-card-footer/post-card-footer.component.ts
@@ -1,16 +1,11 @@
-import {Component, Input, OnInit} from "@angular/core";
-import { PostModel } from "../models/post.model";
+import {Component, Input} from "@angular/core";
+import {PostModel} from "../models/post.model";
 
 @Component({
   selector: 'app-post-card-footer',
   templateUrl: './post-card-footer.component.html',
   styleUrls: ['post-card-footer.component.scss']
 })
-export class PostCardFooterComponent implements OnInit {
+export class PostCardFooterComponent {
   @Input() post!: PostModel;
-    
-    ngOnInit(): void {
-        console.log(222,this.post)
-    }
-
 }
