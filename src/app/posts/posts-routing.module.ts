8bb5a9f59b24d96685e8f36e4a00d9fe9b838891
AnahitKhachatryan <anahitxachatryan11@gmail.com>
2022-03-20 22:09:29 +0400

    fix

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PostsAddEditComponent } from './posts-add-edit/posts-add-edit.component';
-import { PostsViewComponent } from './posts-view/posts-view.component';
-import { PostsComponent } from './posts.component';
+import { PostsAddEditComponent } from '../modules/posts/components/posts-add-edit/posts-add-edit.component';
+import { PostsViewComponent } from '../modules/posts/components/posts-view/posts-view.component';
+import { PostsComponent } from '../modules/posts/posts.component';
 
 const routes: Routes = [
     {
