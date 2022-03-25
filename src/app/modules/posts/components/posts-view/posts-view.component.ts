0751ce5bf4aa551b7bbb20b03a1c5860aa9197e0
AnahitKhachatryan <anahitxachatryan11@gmail.com>
2022-03-25 23:29:23 +0400

    add post view page and style fix

--- src/app/modules/posts/components/posts-view/posts-view.component.ts
+++ src/app/modules/posts/components/posts-view/posts-view.component.ts
@@ -1,4 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { finalize, Subject, takeUntil } from 'rxjs';
+import { PostModel } from 'src/app/modules/post-card/models/post.model';
+import { PostsService } from 'src/app/modules/post-card/services/posts.service';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-posts-view',
@@ -6,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-view.component.scss']
 })
 export class PostsViewComponent implements OnInit {
+  private unsubscribe$ = new Subject<void>();
+  private postId: number | null = null;
+  isLoading = false;
+  post?: PostModel;
 
-  constructor() {}
+  constructor(private postService: PostsService,
+    private activatedRoute: ActivatedRoute,
+    private notifyService: NotificationService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getParams();
+  }
+
+  private getParams(): void {
+    this.activatedRoute.params
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(params => {
+        this.postId = +params['id'];
+        if (this.postId) {
+          this.getPostById();
+        }
+      });
+  }
+
+  private getPostById(): void {
+    if (this.postId) {
+      this.isLoading = true;
+      this.postService.getPostById(this.postId).pipe(
+        takeUntil(this.unsubscribe$),
+        finalize(() => {
+          this.isLoading = false;
+        }))
+        .subscribe({
+          next: (data: PostModel) => {
+            this.post = data;
+            console.log('dataModel', this.post)
+          },
+          error: (err: HttpErrorResponse) => {
+            this.notifyService.showNotification(false, err.error.message);
+          }
+        })
+    }
+  }
 
 }
--- src/app/modules/posts/posts.module.ts
+++ src/app/modules/posts/posts.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {PostsComponent} from './posts.component';
 import {PostsRoutingModule} from './posts-routing.module';
 import {PostsAddEditComponent} from './components/posts-add-edit/posts-add-edit.component';
 import {PostsViewComponent} from './components/posts-view/posts-view.component';
 import {PostCardModule} from "../post-card/post-card.module";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     PostsRoutingModule,
     PostCardModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ]
 })
 export class PostsModule { }
