b184d88db46471484f7aeb06ab79e810e79823d7
Armenuhi97 <armenuhimanukyan@annaniks.com>
2022-04-05 22:51:39 +0400

    change loading

--- src/app/modules/loader/ components/loader/loader.component.ts
+++ src/app/modules/loader/ components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
@@ -6,4 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent {
+  @Input()
+  bigLoader: boolean = false;
 }
--- src/app/modules/loader/loader.module.ts
+++ src/app/modules/loader/loader.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from "@angular/core";
-import {LoaderComponent} from "./ components/loader/loader.component";
-import {LoadingSpinnerComponent} from "./ components/loading-spinner/loading-spinner.component";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { LoaderComponent } from "./ components/loader/loader.component";
+import { LoadingSpinnerComponent } from "./ components/loading-spinner/loading-spinner.component";
 
 @NgModule({
   declarations: [
     LoaderComponent,
     LoadingSpinnerComponent
   ],
-  imports: [],
+  imports: [CommonModule],
   exports: [
     LoaderComponent,
     LoadingSpinnerComponent
